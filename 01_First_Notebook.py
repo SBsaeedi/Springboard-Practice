d14bffac7bd6f23ffc5ae83f1ac095e67382c03e
Saba Saeedi <57029679+SBsaeedi@users.noreply.github.com>
2022-08-21 15:05:53 -0700

    First Anaconda-Jupiter notebook exercise

---
+++ 01_First_Notebook.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# This is my *first* notebook.
+
+# In[1]:
+
+
+x = 5
+y = 6
+z = x + y
+
+
+# In[2]:
+
+
+z
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
